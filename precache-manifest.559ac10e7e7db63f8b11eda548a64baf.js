10fc503292f44b38256f51bf0b6d1917b45b4f82
JavaidAhmadKhan <relaxjavaid@gmail.com>
2020-08-12 19:29:34 +0530

    Updates

---
+++ precache-manifest.559ac10e7e7db63f8b11eda548a64baf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fe45db3b7a6f08c4e56d0e368afaaac",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "75798ae21cd9c67c5b7d",
+    "url": "/React-Portfolio/static/css/main.fa1c83c6.chunk.css"
+  },
+  {
+    "revision": "7affca2f275cb2890cb1",
+    "url": "/React-Portfolio/static/js/2.248ec7c0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React-Portfolio/static/js/2.248ec7c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75798ae21cd9c67c5b7d",
+    "url": "/React-Portfolio/static/js/main.d11af885.chunk.js"
+  },
+  {
+    "revision": "bcebe2d38e8b197ca4b9",
+    "url": "/React-Portfolio/static/js/runtime-main.266d4783.js"
+  },
+  {
+    "revision": "fff3a8d0a2caf86e85d307b7b80b0a64",
+    "url": "/React-Portfolio/static/media/portfolio.fff3a8d0.png"
+  }
+]);
